0c3172a484dac0aa1972c97ebef1710d4eacb466
Rahim <rahimjs@Kali.esi-sba.dz>
2026-06-10 20:40:03 +0100

    Upload full project code

--- backend/server.js
+++ backend/server.js
@@ -65,9 +65,10 @@ app.use('/api/services',servicesRouter);
 app.use('/api/booking',bookingRouter);
 app.use('/api/contact',contactRouter);
 
+
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
     });
